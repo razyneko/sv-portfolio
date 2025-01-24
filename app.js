d71b33e940c3ce3c3ff19391298a009d15b22727
razyneko <139580532+INFERNO1211@users.noreply.github.com>
2025-01-25 00:46:40 +0530

    added some js animations

---
+++ app.js
@@ -0,0 +1,91 @@
+function smoothScroll() {
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+smoothScroll();
+
+function pointerFollower() {
+    window.addEventListener('mousemove', (dets) => {
+        gsap.to('#pointer', {
+            x: dets.clientX,
+            y: dets.clientY,
+        })
+    })
+}
+
+pointerFollower();
+
+function navLinksAnimation() {
+  
+      
+}
+
+navLinksAnimation();
+
+function heroAnimation() {
+
+    const tl = gsap.timeline();
+
+    tl.from('#logo', {
+        y: -20,
+        opacity: 0,
+        duration: 0.7,
+    })
+
+    tl.from('#nav-list h4', {
+        y: -10,
+        opacity: 0,
+        duration: 0.7,
+    })
+    
+    tl.from('#hero-img', {
+        y: 20,
+        opacity: 0,
+        duration: 0.7,
+    })
+
+    tl.from('#data', {
+        y: -30,
+        duration:0.7,
+        opacity: 0,
+    })
+
+    tl.from('#analyst', {
+        y: 30,
+        duration: 0.7,
+        opacity: 0,
+    })
+
+
+
+}
+
+heroAnimation();
